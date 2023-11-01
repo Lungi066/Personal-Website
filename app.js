dbb7195bfbed4f1f3bab0439c975d3882cb544ba
Lungisani Ngcobo <mapholoba066@gmail.com>
2023-11-01 11:57:43 +0200

    add formspree to form

--- app.js
+++ app.js
@@ -41,5 +41,8 @@ function pageTransitions(){
         element.classList.toggle('light-mode')
     })
 }
+document.getElementById('formSubmit').onclick = function() {
+        document.getElementById('formSubmit').submit();
+    }
 
 pageTransitions();
